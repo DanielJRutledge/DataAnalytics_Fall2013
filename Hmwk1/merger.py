40016f09de34d920c3c52a7a8e5ced83ec45b018
EliMor <sergf89@gmail.com>
2013-10-16 19:20:13 -0500

    datas

---
+++ Hmwk1/merger.py
@@ -0,0 +1,25 @@
+import os, sys
+
+#script to find header of a file and merge a whole bunch of files together into one
+
+
+def main():
+	files = os.listdir(".")
+	files = filter(lambda i: "csv" in i.lower(), files)
+	print "Which of the following csv files shall I merge (separate input by commas): "+str(files)
+	selection = raw_input("Selection: ").split(",")
+	selection = [int(i) for i in selection]
+	useFiles =  [i for i in files if files.index(i) in selection]
+	openfiles = [open(i).read().strip().split('\n') for i in useFiles]
+	header = openfiles[0][0]
+	cleanFiles = [d[1:] for d in openfiles]
+	outf =""
+	for datafile in cleanFiles:
+		for row in datafile:
+			outf += row
+			outf += "\n"
+	outfile = open("MergedOutfile.csv","w")
+	outfile.write(header)
+	outfile.write("\n")
+	outfile.write(outf)
+main()
