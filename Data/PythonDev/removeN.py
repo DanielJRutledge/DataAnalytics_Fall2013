589d2186fbe04cca0ba9826fbeaeb86257d1758c
EliMor <sergf89@gmail.com>
2013-10-20 01:12:32 -0500

    RemoveElementUpdate

--- Data/PythonDev/removeN.py
+++ Data/PythonDev/removeN.py
@@ -5,6 +5,8 @@ import os,sys
 
 
 def main():
+	#python removeN.py file
+	#currently only removes the first element in the data file. Will remove and element in the future. 
 	console = sys.argv[1:]
 	csvFile = console[0]
 	# file --removeCol n
@@ -15,13 +17,13 @@ def main():
 
 	data = convertCsvToLists.convert(csvFile)
 	#lazy remove first
-	data = [line[1:] for line in data if n==0]
-	header = data[0]
-	data = data[1:]
-	string = convertListToCsv.convert(header,data)
-	print string
-	outfile = open("dataMinusFirst.csv","w")
-	outfile.write(string)
+	if n == 0:
+		data = [line[1:] for line in data if n==0]
+		header = data[0]
+		data = data[1:]
+		string = convertListToCsv.convert(header,data)
+		outfile = open("dataMinusFirst.csv","w")
+		outfile.write(string)
 
 main()
 
